2324de5b932392b79a27d79bf2eb3b10db305aa9
Carlos Leyva <carlosleyva195@gmail.com>
2021-12-30 16:04:31 -0700

    Enviroment prod listo1

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use( '/api/auth', require('./routes/auth'));
 
 //Manejar demas rutas
 app.get( '*', ( req, res ) => {
-    res.sendFile( path.resolve(__dirname, 'public/index.html') );
+    res.sendFile( path.join(__dirname, '/public/index.html') );
 })
 
 app.listen( process.env.PORT, () => {
