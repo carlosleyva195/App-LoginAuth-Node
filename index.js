96928d0985992bc443f566b1a6268b77a828dc48
Carlos Leyva <carlosleyva195@gmail.com>
2021-12-30 14:10:12 -0700

    make it better

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const { dbConnnection } = require('./db/config');
+require('dotenv').config();
+
+//Crear el servidor/aplicación de express
+const app = express();
+
+//Base de datos
+dbConnnection();
+
+//Directorio publico
+app.use( express.static('public'));
+
+//CORS
+app.use( cors() );
+
+//Lectura y parseo del body
+app.use( express.json() );
+
+//Rutas
+app.use( '/api/auth', require('./routes/auth'));
+
+//Manejar demas rutas
+app.get( '*', ( req, res ) => {
+    res.sendFile( path.resolve(__dirname, 'public/index.html') );
+})
+
+app.listen( process.env.PORT, () => {
+    console.log( `Servidor corriendo en puerto ${ process.env.PORT }` );
+});
